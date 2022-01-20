e037544d84cdc0def5fcbc2b3d730a7c6873d940
Rafael <rafael.mendes1101@gmail.com>
2022-01-20 18:06:00 +0000

    Coding List

--- travel_app/src/components/List/List.jsx
+++ travel_app/src/components/List/List.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  Select,
+} from "@material-ui/core";
 
+import useStyles from "./styles";
 const List = () => {
-  return <h1>List</h1>;
+  const classes = useStyles();
+  const [type, setType] = useState("restaurants");
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurants, Hotels and Attractions around you
+      </Typography>
+      <FormControl classNmae={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 
 export default List;
